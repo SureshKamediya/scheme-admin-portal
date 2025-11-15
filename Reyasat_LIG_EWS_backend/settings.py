151bef95f633db31a01682a6291f035e06f047ba
SureshKamediya <sureshkamediya18@gmail.com>
2025-11-15 19:21:02 +0530

    Update to local Postgres Database

--- Reyasat_LIG_EWS_backend/settings.py
+++ Reyasat_LIG_EWS_backend/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'Reyasat_LIG_EWS_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scheme_1_db',  # Use the DB name you created
+        'USER': 'suresh',    # Use the user you created
+        'PASSWORD': 'surya@18', # Use the password you created
+        'HOST': 'localhost',      # If running locally
+        'PORT': '5432',           # Default PostgreSQL port
     }
 }
 
